75c0917297b242d24043eb95c272110972f0fc1a
Sergio Pinzon <spinzonr@compensar.com>
2025-10-13 18:07:52 -0500

    fix: estructura base de MainActivity, menú y header

--- app/src/main/java/com/example/huelllitas/MainActivity.kt
+++ app/src/main/java/com/example/huelllitas/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.example.huelllitas
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var drawerLayout: DrawerLayout
+//    private lateinit var navigationView: NavigationView
+//    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
 
-        }, 3000)
+//        initViews()
+//        setupSharedPreferences()
+//        setupNavigationView()
 
     }
+
+//    private fun initViews() {
+//        drawerLayout = findViewById(R.id.main)
+//        navigationView= findViewById(R.id.main)
+//
+//
+//
+//    }
 }
