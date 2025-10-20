9eff017d0b68ec957b5d0dc54fa5ed8dcb05fe31
Camilo1327 <peraltacamilo1@gmail.com>
2025-10-19 20:14:24 -0500

    Se realizan ajustes visuales en el login

---
+++ app/src/main/java/com/example/huelllitas/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.huelllitas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstance: Bundle?){
+        super.onCreate(savedInstance)
+        setContentView(R.layout.activity_login)
+    }
+}
