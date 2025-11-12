30edb6e6baf05fde35b6412e469d1d203faab501
Sergio Pinzon <spinzonr@compensar.com>
2025-11-12 16:13:13 -0500

    feature: login google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -42,6 +43,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/huelllitas/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/huelllitas/fragments/LoginFragment.kt
@@ -1,7 +1,9 @@
 package com.example.huelllitas.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +14,19 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.huelllitas.R
 import com.example.huelllitas.utils.PreferencesManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlin.text.replace
 
+@Suppress("DEPRECATION")
 class LoginFragment : Fragment() {
-
+    private lateinit var btnGoogle: Button
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
     private lateinit var preferencesManager: PreferencesManager
     private lateinit var emailInput: EditText
     private lateinit var passwordInput: EditText
@@ -23,6 +34,11 @@ class LoginFragment : Fragment() {
     private lateinit var registerText: TextView
 
     private lateinit var recoveryPassword: TextView
+
+    companion object{
+        private const val TAG="LoginFragment"
+        private const val RC_SIGN_IN = 9001
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,8 +51,25 @@ class LoginFragment : Fragment() {
         initViews(view)
         setupListeners()
 
+        btnGoogle=view.findViewById(R.id.btnGoogle)
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("1080398941603-ddbg9e2ol8jt7b2tg0d90o743robvuou.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient=GoogleSignIn.getClient(requireActivity(),gso)
+
+        btnGoogle.setOnClickListener {
+            signIn()
+        }
+
+
         return view
     }
+
+
+
 
     private fun initViews(view: View) {
         emailInput = view.findViewById(R.id.editTextEmail)
@@ -112,6 +145,57 @@ class LoginFragment : Fragment() {
             .addToBackStack(null)
             .commit()
     }
+    private fun signIn(){
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()){ task ->
+                if (task.isSuccessful){
+                    Log.d(TAG,"signInWithCredential:success")
+                    Toast.makeText(context,"Bienvenido", Toast.LENGTH_SHORT).show()
+                    parentFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_content, HomeFragment())
+                        .addToBackStack(null)
+                        .commit()
+                } else {
+                    Log.w(TAG, "signinWithCredential:failure", task.exception)
+                    Toast.makeText(context,"Error: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//        val currentUser = auth.currentUser
+//        if (currentUser != null){
+//            parentFragmentManager.beginTransaction()
+//                .replace(R.id.fragment_content, HomeFragment())
+//                .addToBackStack(null)
+//                .commit()
+//        }
+//    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val accountData = task.getResult(ApiException::class.java)!!
+                Log.d(TAG,"firebaseAuthWithGoogle:"+ accountData.id)
+                firebaseAuthWithGoogle(accountData.idToken!!)
+            } catch (e: ApiException){
+                Log.w(TAG, "Google sign in failed", e)
+                Toast.makeText(context,"Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+
+
 
 
